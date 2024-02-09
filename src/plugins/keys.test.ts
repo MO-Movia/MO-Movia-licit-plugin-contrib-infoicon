877150a42568f14f5346d8f1499b2192ee153467
Ryan Culp <rculp@modusoperandi.com>
2024-02-09 12:48:41 -0500

    update dependencies

--- src/plugins/keys.test.ts
+++ src/plugins/keys.test.ts
@@ -1,9 +1,9 @@
-import { marks } from './keys';
+import {Mark} from 'prosemirror-model';
+import {marks} from './keys';
 
 describe('ProseMirror marks', () => {
-    const emMark = marks.em as any;
-    test('serializes to em tag', () => {
-        const emNode = emMark.toDOM();
-        expect(emNode).toEqual(['em', 0]);
-    });
+  test('serializes to em tag', () => {
+    const emNode = marks.em?.toDOM?.({} as unknown as Mark, false);
+    expect(emNode).toEqual(['em', 0]);
+  });
 });
