a9ebcf20afaef42937fb7a0740539c20ed94be38
Amina <amina.sharaf@firmusoft.co.in>
2025-11-28 18:52:16 +0530

    Issue fix : The icon of Info icon is not showing properly

--- src/images.ts
+++ src/images.ts
@@ -1,4 +1,4 @@
-const DarkThemeIcon = '/assets/images/dark/Icon_Infoicon.svg';
-const LightThemeIcon = '/assets/images/light/Icon_Infoicon.svg';
+const DarkThemeIcon = 'assets/images/dark/Icon_Infoicon.svg';
+const LightThemeIcon = 'assets/images/light/Icon_Infoicon.svg';
 
 export {DarkThemeIcon, LightThemeIcon};
